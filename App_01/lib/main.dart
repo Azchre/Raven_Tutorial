a74d40565b35ace8795150f41b63923d6fbdf1b6
Edward Molina <edwardmolina321@gmail.com>
2021-01-30 15:08:04 +0800

    Added Feature For Changing Names

--- App_01/lib/main.dart
+++ App_01/lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
-import 'screen/screen.dart';
-=======
 import 'screens/screen.dart';
->>>>>>> 9f2606d28319375eca9f4d86c283d6540e9072a4
 
 void main() {
   runApp(MyApp());
